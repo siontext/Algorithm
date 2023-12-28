d21b1620325d1fe8e3c6daec45e59959cc648fa8
sion Jeong <151036497+siontext@users.noreply.github.com>
2023-12-28 22:50:56 +0900

    [level unrated] Title: 문자열 출력하기, Time: 135.67 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner 스캐너 = new Scanner(System.in); //스캐너 객체 만들기
-        String a = 스캐너.next();
-         //next()" 메서드는 공백 기준으로 문자열을 분할하고 다음 단어를 반환
-        
-        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
     }
 }
