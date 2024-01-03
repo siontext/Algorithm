7ad7b158cb226447f85b8722eac83acc3e78f8f3
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-01-03 13:16:16 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 114.00 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String A = "!@#$%^&*(\\'\"<>?:;";
+        System.out.print(A);
+    }
+
+    
+    //"  !@#"!@#" (쌍따옴표에서 문자열이 끊기게됨  \ 백슬래쉬를 붙혀줘야한다)
+    // ' " 은 \을 붙혀줘야한다.
+    //정답보고 품
+}
