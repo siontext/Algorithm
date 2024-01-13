9942b0abd9120b005159c6ba8cba30e670b02c4b
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-01-13 22:02:17 +0900

    [level Lv.0] Title: n의 배수, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num, int n) {
+        
+        if(num % n == 0) {
+            int a = 1;
+            return a;
+        } else {
+            int b = 0;
+            return b;
+        }
+        
+    }
+}
+
+//if 절 
+//나눠서 나머지 0이 나오면 배수가 맞는거고, 나머지1이 나오면 배수가 아닌걸로 식을 만들면 될 것 같다. 
+//그 어떨때는 return을 쓰고 어떨땐 sout를 쓰는지 배웠던거 같은데 까먹었다..... 뭐였지...?
