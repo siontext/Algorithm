080a1ea7ecc56302723b62c3e42854efc4d9a3c4
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-06-20 11:13:38 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String message) {
+        int count = message.length();
+        
+        int result = count * 2;
+        
+    
+        return result;
+    }
+}
+
+//글자 몇개인지 카운트? 근데 공백은 어케 처리?
+// 그리고 문자수 x2하면 되긴하는데....
