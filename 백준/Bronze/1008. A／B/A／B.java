077e2b619a32022abcf4737735f57a96a88f6d02
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-09-23 20:51:43 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,14 +1,16 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
 
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-
-        System.out.println(A / B);
+        System.out.println(a/b);
 
     }
+
 }
