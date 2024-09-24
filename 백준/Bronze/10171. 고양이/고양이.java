77ddca0bb6e98fd93cc46bbc6cbdd2920ceb33f5
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-09-24 19:50:14 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+
+
+
+        System.out.println("\\    /\\ ");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+
+    //주의사항 역슬래시(\) 는 이스케이프 문자로 프로그래밍 언어에서 특수한 의미를 갖음
+}
