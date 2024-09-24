1fb4b819c32fbe5b2debc1a57cd5bb8615794cd0
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-09-24 20:57:00 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args) {
+
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+
+
+
+    //^는 XOR 연산자 (이진수 비교)
+    //""가 함께 있으면 문자열 취급을 해버림
+}
