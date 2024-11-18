34ada106480496a74269a870d5c036113bd80d46
sion Jeong <151036497+siontext@users.noreply.github.com>
2024-11-18 10:45:56 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-
 public class Main{
     public static void main(String[] args){
         
-        Scanner sc =  new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
-        int N = sc.nextInt();// 정수 개수 입력
-        int[] numbers = new int[N]; //길이가 N인 배열 생성
+        int N = sc.nextInt();
         
-        // 배열에 정수 입력 받기
-        for(int i=0; i < N; i++){
-            numbers[i] = sc.nextInt();
+        int[] nums = new int[N]; // N개의 정수를 저장할 배열 선언
+        
+        // 배열에 N개의 정수 입력받기
+        for(int i=0; i<N; i++){
+            nums[i] = sc.nextInt();
         }
         
-        //셋째 줄 찾으려고 하는 정수 입력
-        int x = sc.nextInt();
-        int count = 0; // 찾으려는 v의 개수
+        int V = sc.nextInt();
+        int count = 0;
         
         for(int i=0; i<N; i++){
-            if(x == numbers[i]){
-                count ++;
+            if(V == nums[i]){
+                count++;
             }
         }
         
         System.out.println(count);
-       
+        
+        
     }
-}
+} 
